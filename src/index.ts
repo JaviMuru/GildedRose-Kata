fb69995a36e7238c82ac470c04049ce9cc092651
Javi Muruzábal <javimuruzabal@iMac-Javi.lan>
2021-05-26 22:16:37 +0200

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import {Item} from "./app/Item";
-import {GildedRose} from "./app/GildedRose";
-import {ItemFactory} from "./app/ItemFactory";
+import { GildedRose } from "./app/GildedRose"
+import { ItemFactory } from "./app/Item/ItemFactory"
 
 const main = () => {
   const item = new ItemFactory("whatever", 10, 0)
 
   const gildedRose = new GildedRose()
-  // @ts-ignore
   gildedRose.updateQuality([item])
 
   console.log('gildedRose',gildedRose)
